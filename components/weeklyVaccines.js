576bd9f0e546dfb024743780e05ff7e5df128143
Chris Howson <chris.howson@valtech.com>
2021-03-30 11:59:19 +0100

    Manually calculating weekly vaccine doses

--- components/weeklyVaccines.js
+++ components/weeklyVaccines.js
@@ -17,11 +17,50 @@ function WeeklyVaccines() {
             series: []
         }
 
+        var sortedData = _.sortBy(vaccineData.data, 'date');
+        
+        let weeklyFirstDoseEntries = [];
+        let weeklySecondDoseEntries = [];
+        let weeklyFirstDoseCount = 0;
+        let weeklySecondDoseCount = 0;
+        sortedData.forEach(entry => {
+            if(!entry.newPeopleVaccinatedFirstDoseByPublishDate && !entry.newPeopleVaccinatedSecondDoseByPublishDate) {
+                if(entry.weeklyPeopleVaccinatedFirstDoseByVaccinationDate) {
+                    weeklyFirstDoseEntries.push([ToUtc(new Date(entry.date)), entry.weeklyPeopleVaccinatedFirstDoseByVaccinationDate])
+                }
+                if(entry.weeklyPeopleVaccinatedSecondDoseByVaccinationDate) {
+                    weeklySecondDoseEntries.push([ToUtc(new Date(entry.date)), entry.weeklyPeopleVaccinatedSecondDoseByVaccinationDate])
+                }
+            }
+            else {
+                if(!!entry.newPeopleVaccinatedFirstDoseByPublishDate) {
+                    weeklyFirstDoseCount += entry.newPeopleVaccinatedFirstDoseByPublishDate;
+                }
+
+                if(!!entry.newPeopleVaccinatedSecondDoseByPublishDate) {
+                    weeklySecondDoseCount += entry.newPeopleVaccinatedSecondDoseByPublishDate;
+                }
+
+                if(new Date(entry.date).getDay() == 0) {
+                    // Sunday push entry and reset
+                    weeklyFirstDoseEntries.push([ToUtc(new Date(entry.date)), weeklyFirstDoseCount]);
+                    weeklySecondDoseEntries.push([ToUtc(new Date(entry.date)), weeklySecondDoseCount]);
+
+                    weeklyFirstDoseCount = 0;
+                    weeklySecondDoseCount = 0;
+                }
+            }
+        });
+        
+        const lastEntry = sortedData[sortedData.length - 1];
+        if(new Date(lastEntry.date).getDay() !== 0) {
+            weeklyFirstDoseEntries.push([ToUtc(new Date(lastEntry.date)), weeklyFirstDoseCount]);
+            weeklySecondDoseEntries.push([ToUtc(new Date(lastEntry.date)), weeklySecondDoseCount]);
+        }
+
         weeklyVaccineSeries.series.push({
             name: "Weekly First Dose Vaccines",
-            data: vaccineData.data
-                        .filter(day => day.weeklyPeopleVaccinatedFirstDoseByVaccinationDate !== null)
-                        .map((day => [ToUtc(new Date(day.date)), day.weeklyPeopleVaccinatedFirstDoseByVaccinationDate])),
+            data: weeklyFirstDoseEntries,
             color: graphColours[0],
             type: 'column',
             stacking: true
@@ -29,13 +68,31 @@ function WeeklyVaccines() {
 
         weeklyVaccineSeries.series.push({
             name: "Weekly Second Dose Vaccines",
-            data: vaccineData.data
-                        .filter(day => day.weeklyPeopleVaccinatedSecondDoseByVaccinationDate !== null)
-                        .map((day => [ToUtc(new Date(day.date)), day.weeklyPeopleVaccinatedSecondDoseByVaccinationDate])),
+            data: weeklySecondDoseEntries,
             color: graphColours[1],
             type: 'column',
             stacking: true
         });
+
+        // weeklyVaccineSeries.series.push({
+        //     name: "Weekly First Dose Vaccines",
+        //     data: vaccineData.data
+        //                 .filter(day => day.weeklyPeopleVaccinatedFirstDoseByVaccinationDate !== null)
+        //                 .map((day => [ToUtc(new Date(day.date)), day.weeklyPeopleVaccinatedFirstDoseByVaccinationDate])),
+        //     color: graphColours[0],
+        //     type: 'column',
+        //     stacking: true
+        // });
+
+        // weeklyVaccineSeries.series.push({
+        //     name: "Weekly Second Dose Vaccines",
+        //     data: vaccineData.data
+        //                 .filter(day => day.weeklyPeopleVaccinatedSecondDoseByVaccinationDate !== null)
+        //                 .map((day => [ToUtc(new Date(day.date)), day.weeklyPeopleVaccinatedSecondDoseByVaccinationDate])),
+        //     color: graphColours[1],
+        //     type: 'column',
+        //     stacking: true
+        // });
 
         series.push(weeklyVaccineSeries);
         
